39e895ec8b5d92a2ee63922b61c11876a0438a02
evenhumble <patrickwuke@163.com>
2017-09-11 13:52:14 +0800

    add io_examples todo list

--- subprocess/shell_command_examples.py
+++ subprocess/shell_command_examples.py
@@ -3,16 +3,16 @@ import subprocess
 result = subprocess.call('date')
 print(result)
 
-print("\nToday is",end=" ",flush=True)
-subprocess.call(['date','-u','+%A'])
+print("\nToday is", end=" ", flush=True)
+subprocess.call(['date', '-u', '+%A'])
 # grep ,grep -i
-cwd=subprocess.getoutput('pwd')
+cwd = subprocess.getoutput('pwd')
 print(cwd)
 
-(ls_status,ls_output)=subprocess.getstatusoutput('ls helloworld')
-print(ls_status,ls_output)
+(ls_status, ls_output) = subprocess.getstatusoutput('ls helloworld')
+print(ls_status, ls_output)
 
 ## subprocess return status
 
-ls_status = subprocess.call('ls helloworld',stderr=subprocess.DEVNULL,shell=True)
+ls_status = subprocess.call('ls helloworld', stderr=subprocess.DEVNULL, shell=True)
 print(ls_status)
