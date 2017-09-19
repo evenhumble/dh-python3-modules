295f36902716a3eca8750776af88bd4ac3e6c3f3
evenhumble <patrickwuke@163.com>
2017-09-19 11:20:57 +0800

    add collections samples

--- collection_samples/chainmap_example.py
+++ collection_samples/chainmap_example.py
@@ -4,7 +4,7 @@ import collections
 a = {'a': 'A', 'c': 'C'}
 b = {'b': 'B', 'c': 'D'}
 
-m = collections.ChainMap(a,b)
+m = collections.ChainMap(a, b)
 
 print('Individual Values')
 print('a = {}'.format(m['a']))
@@ -16,3 +16,26 @@ print('Values = {}'.format(list(m.values())))
 
 print('Items:')
 
+for k, v in m.items():
+    print('{}={}'.format(k, v))
+
+print('"d" in m : {}'.format(('d' in m)))
+
+print("c={}".format(m['c']))
+## sorting
+m.maps = list(reversed(m.maps))
+print(m.maps)
+print("c={}".format(m['c']))
+
+# This stacking behavior is what makes it convenient to
+# use ChainMap instances as template or application contexts.
+# Specifically, it is easy to add or update values in one iteration,
+# then discard the changes for the next iteration.
+
+m2 = m.new_child()
+print(m)
+print(m2)
+m2['c']='E'
+
+print(m)
+print(m2)
