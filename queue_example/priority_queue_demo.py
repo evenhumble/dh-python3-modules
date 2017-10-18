b17d91705857d2050bb939bfd6b65a9465975c47
evenhumble <patrickwuke@163.com>
2017-10-18 15:39:00 +0800

    add queue demo

--- queue_example/priority_queue_demo.py
+++ queue_example/priority_queue_demo.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 import functools
 import queue
+import random
 import threading
+import time
 
 
 @functools.total_ordering
@@ -24,9 +26,9 @@ class Job:
 
 
 q = queue.PriorityQueue()
-
 q.put(Job(3, 'Mid-level job'))
-q.put(Job(10, 'Low-level job'))
+q.put(Job(10, 'Low-level-10 job'))
+q.put(Job(20, 'Low-level-20 job'))
 q.put(Job(1, 'Important job'))
 
 
@@ -34,10 +36,13 @@ def process_job(q):
     while True:
         next_job = q.get()
         print('Processing job:', next_job.description)
+        print(random.randint(1, 10))
+        time.sleep(random.randint(1, 10))
         q.task_done()
 
 
 workers = [
+    threading.Thread(target=process_job, args=(q,)),
     threading.Thread(target=process_job, args=(q,)),
     threading.Thread(target=process_job, args=(q,))
 ]
