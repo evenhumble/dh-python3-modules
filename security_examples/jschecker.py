cda3a091ebce74a42b872b975805c831ddff4837
evenhumble <patrickwuke@163.com>
2017-12-06 21:32:56 +0800

    add more security samples

--- security_examples/jschecker.py
+++ security_examples/jschecker.py
@@ -7,8 +7,7 @@ import sys
 scripts = []
 
 if len(sys.argv) != 2:
-    print
-    "usage: %s url" % (sys.argv[0])
+    print("usage: %s url" % (sys.argv[0]))
     sys.exit(0)
 
 tarurl = sys.argv[1]
@@ -20,8 +19,7 @@ for line in soup.find_all('script'):
 
 for script in scripts:
     if "jquery.min" in str(script).lower():
-        print
-        script
+        print(script)
         url = requests.get(script)
         comments = re.findall(r'\d[0-9a-zA-Z._:-]+', url.text)
         if comments[0] == "2.1.1" or comments[0] == "1.12.1":
