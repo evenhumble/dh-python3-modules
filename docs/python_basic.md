f62ea394113b097f15f352e3b3f2b460204b3c81
evenhumble <patrickwuke@163.com>
2018-01-06 19:50:29 +0800

    add python tips demo

---
+++ docs/python_basic.md
@@ -0,0 +1,33 @@
+# Python Steps 
+
+- Basic
+- list/tuple
+- string
+- dict
+- for/loop/condition
+- abstraction/oo
+- exception
+- modules
+- magic method/property/generator/iterator
+- file/io/stream
+- gui
+- database
+
+## Abstraction
+
+- abstraction
+- function definition
+- arguments
+- scope
+- recursion
+- functional programing
+  * lambda
+  * map
+  * filter
+  * reduce
+  * sum
+  * apply
+- polymorphism
+- encapsulation
+- inheritance
+- duck typing
--- tips_example/todictmixin_demo.py
+++ tips_example/todictmixin_demo.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+import json
+
 
 class ToDictMixin(object):
     def to_dict(self):
@@ -30,7 +32,44 @@ class BinaryTree(ToDictMixin):
         self.left = left
         self.right = right
 
+
 tree = BinaryTree(10, left=BinaryTree(7, right=BinaryTree(9)),
-    right=BinaryTree(13, left=BinaryTree(11)))
+                  right=BinaryTree(13, left=BinaryTree(11)))
 
 print(tree.to_dict())
+
+
+class JsonMixin(object):
+    @classmethod
+    def from_json(cls, data):
+        kwargs = json.loads(data)
+        return cls(**kwargs)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+
+class DatacenterRack(ToDictMixin, JsonMixin):
+    def __init__(self, switch=None, machines=None):
+        self.switch = Switch(**switch)
+        self.machines = [Machine(**kwags) for kwags in machines]
+
+
+class Switch(ToDictMixin, JsonMixin):
+    pass
+
+
+class Machine(ToDictMixin, JsonMixin):
+    pass
+
+
+serialized = """{
+    "switch": {"ports": 5, "speed": 1e9},
+    "machines": [
+        {"cores": 8, "ram": 32e9, "disk": 5e12},
+        {"cores": 4, "ram": 16e9, "disk": 1e12},
+        {"cores": 2, "ram": 4e9, "disk": 500e9}
+] }"""
+deserialized = DatacenterRack.from_json(serialized)
+
+print(deserialized)
