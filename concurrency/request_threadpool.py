41d4ff86c59e2caf2093f0b700a0e318f6c9a8fe
patrick <patrick@pwlocal.local>
2018-09-14 09:22:38 +0800

    add thread pool

--- concurrency/request_threadpool.py
+++ concurrency/request_threadpool.py
@@ -8,6 +8,7 @@ HTTPBIN_URL = "http://httpbin.org/get?a={}"
 COUNTS = range(12)
 
 
+
 def fetch(a):
     r = requests.get(HTTPBIN_URL.format(a))
     return r.json()['args']['a']
@@ -17,5 +18,5 @@ start = time.time()
 with ThreadPoolExecutor(max_workers=3) as executor:
     for num, result in zip(COUNTS, executor.map(fetch, COUNTS)):
         print("fetch({})={}".format(num, result))
-
+        
 print('Use requests+ThreadPoolExecutor cost: {}'.format(time.time() - start))
